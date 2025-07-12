f77bb0eb6e49daa9c0ac2a70767216943e241a5f
Artem <ploo6669@outlook.com>
2025-07-12 14:15:34 +0300

    init8

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import plugin from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [plugin()],
-    base: "ploo6669.github.io",
+    base: "/ploo6669.github.io",
     server: {
         port: 60992,
     }
