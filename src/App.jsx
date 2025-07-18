2f782c468804b569e76a8586fa5ac960b83a9dbf
Artem <ploo6669@outlook.com>
2025-07-18 18:11:35 +0300

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,27 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const [sum, setSum] = useState(0)
   const [count, setCount] = useState(0)
-
+  function Button (){
+  if (sum%5==0&sum>5) {
+  return(
+        <button onClick={() => {setCount((count) => {count=count+1; console.log(count);}); setSum((sum) => sum+=1)}}>
+          The count is {"V"}
+        </button>
+ )
+  }else if (count%2==0)
+  return(
+        <button onClick={() => {setCount((count) => {count=count+3; console.log(count);}); setSum((sum) => sum+=1)}}>
+          The count is {"O"}
+        </button>
+ )
+   else return(
+        <button onClick={() => {setCount((count) => {count=count+5; console.log(count);}); setSum((sum) => sum+=1)}}>
+          The count is {"X"}
+        </button>
+ )
+}
   return (
     <>
       <div>
@@ -18,10 +37,16 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => !count)}>
-          The count is {count?"X":"O"}
-        </button>
-        <p>
+      <div>
+      <Button/>  
+      </div>
+      {sum>10?(<div>
+      <Button/>  
+      </div>): " "}
+      {sum>20?(<div>
+      <Button/>  
+      </div>): " "}
+     <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
